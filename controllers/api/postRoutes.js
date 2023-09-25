f1372a3101d60f15f2bb00d27603eabcff0ddbfe
SKhai77 <orionkhai@gmail.com>
2023-09-25 16:21:24 -0500

    Modified the code block for new post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -18,7 +18,7 @@ router.get('/edit-post/:id', withAuth, async (req, res) => {
     res.render('edit-post', {
       post: postData.get({ plain: true }),
       logged_in: req.session.logged_in,
-      editing: true, // Set editing to true when rendering the edit post page
+      editing: true,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -28,40 +28,47 @@ router.get('/edit-post/:id', withAuth, async (req, res) => {
 // POST route to create a new post
 router.post('/', withAuth, async (req, res) => {
   try {
+    // Validate incoming data
+    if (!req.body.title || !req.body.content || !req.body.location) {
+      res.status(400).json({ message: 'Title, content, and location are required fields.' });
+      return;
+    }
+
     // Create a new post with the data from the request body
     const newPost = await Post.create({
-      ...req.body,
+      title: req.body.title,
+      content: req.body.content,
+      location: req.body.location,
       user_id: req.session.user_id,
     });
 
     res.status(201).json(newPost);
   } catch (err) {
-    res.status(400).json(err);
+    console.error(err); // Log the error for debugging
+    res.status(500).json({ message: 'Failed to create a new post. Description text is at maximum 255 characters.' });
   }
 });
 
 // PUT route to edit/update a post by ID
 router.put('/:id', withAuth, async (req, res) => {
   try {
-    // Find the post by ID and update its content
-    const updatedPost = await Post.update(
-      {
-        ...req.body,
-      },
-      {
-        where: {
-          id: req.params.id,
-          user_id: req.session.user_id,
-        },
-      }
-    );
+    // Find the post by ID
+    const postData = await Post.findByPk(req.params.id);
 
-    if (!updatedPost[0]) {
+    if (!postData) {
       res.status(404).json({ message: 'No post found with this id!' });
       return;
     }
 
-    res.status(200).json(updatedPost);
+    // Update the post data based on the request body
+    postData.title = req.body.title;
+    postData.content = req.body.content;
+    postData.location = req.body.location;
+
+    // Save the updated post data
+    await postData.save();
+
+    res.status(200).json(postData);
   } catch (err) {
     res.status(500).json(err);
   }

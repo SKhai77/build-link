623697051975d04d7c08e14736e9fba28927588f
SKhai77 <orionkhai@gmail.com>
2023-09-25 02:37:19 -0500

    Add search-results.handlebars file and search function works on the home page and the joblisting page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,12 +1,12 @@
-// Import necessary modules and initialize the Express.js router
-const router = require('express').Router();
-const { Post, User, Comment, } = require('../models');
+const express = require('express');
+const router = express.Router();
+const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
+const { Op } = require('sequelize'); // Import Sequelize's Op for querying
 
 // GET route to render the edit post page
 router.get('/edit-post/:id', withAuth, async (req, res) => {
   try {
-
     // Find the post by ID
     const postData = await Post.findByPk(req.params.id);
 
@@ -19,18 +19,71 @@ router.get('/edit-post/:id', withAuth, async (req, res) => {
     res.render('edit-post', {
       post: postData.get({ plain: true }),
       logged_in: req.session.logged_in,
-      editing: true, // Set editing to true when rendering the edit post page
+      editing: true, 
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// Define a route to handle GET requests for the homepage
+// Route for rendering the homepage with search results or all job listings
 router.get('/', async (req, res) => {
   try {
-    // Get all posts and JOIN with user data
-    const postData = await Post.findAll({
+    const { keywords, location } = req.query;
+
+    if (keywords || location) {
+      const searchData = await Post.findAll({
+        where: {
+          [Op.or]: [
+            { title: { [Op.like]: `%${keywords}%` } },
+            { location: { [Op.like]: `%${location}%` } },
+          ],
+        },
+        include: [
+          {
+            model: User,
+            attributes: ['username'],
+          },
+        ],
+      });
+
+      const posts = searchData.map((post) => post.get({ plain: true }));
+
+      return res.render('homepage', {
+        posts,
+        logged_in: req.session.logged_in,
+        user_name: req.session.user_name,
+      });
+    }
+
+    const posts = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    const serializedPosts = posts.map((post) => post.get({ plain: true }));
+
+    res.render('homepage', {
+      posts: serializedPosts,
+      logged_in: req.session.logged_in,
+      user_name: req.session.user_name,
+    });
+  } catch (err) {
+    // Consider using a centralized error handler
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+// Route for rendering the job listings page
+router.get('/joblisting', async (req, res) => {
+  try {
+    // Fetch all job posts
+    const posts = await Post.findAll({
       include: [
         {
           model: User,
@@ -40,11 +93,11 @@ router.get('/', async (req, res) => {
     });
 
     // Serialize data so the template can read it
-    const posts = postData.map((post) => post.get({ plain: true }));
+    const serializedPosts = posts.map((post) => post.get({ plain: true }));
 
-    // Pass serialized data and session flag into the template
-    res.render('homepage', {
-      posts,
+    // Pass serialized data and session flag into the joblisting template
+    res.render('joblisting', {
+      posts: serializedPosts,
       logged_in: req.session.logged_in,
       user_name: req.session.user_name,
     });
@@ -53,7 +106,7 @@ router.get('/', async (req, res) => {
   }
 });
 
-// Display a specific post and its associated comments
+// Route for displaying a specific post and its associated comments
 router.get('/post/:id', async (req, res) => {
   try {
     // Get the post with associated user and comments
@@ -86,7 +139,7 @@ router.get('/post/:id', async (req, res) => {
   }
 });
 
-// Use withAuth middleware to prevent access to route
+// Route for rendering the user's profile
 router.get('/profile', withAuth, async (req, res) => {
   try {
     // Find the logged-in user based on the session ID
@@ -107,6 +160,7 @@ router.get('/profile', withAuth, async (req, res) => {
   }
 });
 
+// Route for rendering the login page
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
@@ -117,20 +171,64 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-// Add a route for the "About Us" page
+// Route for rendering the "About Us" page
 router.get('/about', (req, res) => {
-  res.render('about'); // This renders the "about.handlebars" template
+  res.render('about');
 });
 
-router.get('/joblisting', (req, res) => {
-  res.render('joblisting'); // This renders the "joblisting.handlebars" template
-});
-
-
+// Route for rendering the "Contact" page
 router.get('/contact', (req, res) => {
-  res.render('contact'); // This renders the "contact.handlebars" template
+  res.render('contact');
 });
 
+// Route for handling search requests
+router.get('/search', async (req, res) => {
+  try {
+    // Get search parameters from the query string
+    const { keywords, location } = req.query;
+
+    // Define an empty object to store the search criteria
+    const searchCriteria = {};
+
+    // If keywords are provided, add them to the search criteria
+    if (keywords) {
+      searchCriteria.title = {
+        [Op.like]: `%${keywords}%`, // Case-insensitive title search
+      };
+    }
+
+    // If location is provided, add it to the search criteria
+    if (location) {
+      searchCriteria.location = {
+        [Op.like]: `%${location}%`, // Case-insensitive location search
+      };
+    }
+
+    // Fetch job posts based on the search criteria
+    const searchData = await Post.findAll({
+      where: searchCriteria,
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+      // Add the COLLATE clause for case-insensitive searching
+      collate: 'utf8_general_ci',
+    });
+
+    const searchResults = searchData.map((post) => post.get({ plain: true }));
+
+    // Render the search-results.handlebars page with the filtered posts
+    res.render('search-results', {
+      searchResults, // Pass the search results to the template
+      logged_in: req.session.logged_in,
+      user_name: req.session.user_name,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // Export the router
 module.exports = router;

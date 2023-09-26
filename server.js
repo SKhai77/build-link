449fc205557ec97317150ff208de7d46d769c5a6
SKhai77 <orionkhai@gmail.com>
2023-09-26 01:44:03 -0500

    Added the bodyParser middleware

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const bodyParser = require('body-parser');
 
 // Import Sequelize and create a connection
 const sequelize = require('./config/connection');
@@ -23,7 +24,7 @@ const sess = {
   cookie: {
     maxAge: 300000, // Session duration in milliseconds
     httpOnly: true,
-    secure: false, // Set to true in a production environment with HTTPS
+    secure: false, // Must set to true in a production environment with HTTPS
     sameSite: 'strict',
   },
   resave: false,
@@ -44,11 +45,19 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Add body-parser middleware for parsing form data
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Use the defined routes
 app.use(routes);
+
+// Add this after your routes
+app.use((req, res) => {
+  res.status(404).send("Not Found");
+});
 
 // Sync Sequelize models with the database and start the server
 sequelize.sync({ force: false }).then(() => {

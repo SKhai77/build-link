2d8331dc7b4296839c088b638d999e570337cece
SKhai77 <orionkhai@gmail.com>
2023-09-30 23:42:15 -0500

    Changed smpt setting

--- controllers/api/contactRoutes.js
+++ controllers/api/contactRoutes.js
@@ -12,7 +12,9 @@ router.post('/', (req, res) => {
 
   // Configure nodemailer to send the email
   const transporter = nodemailer.createTransport({
-    service: 'hotmail',
+    host: 'smtp-mail.outlook.com',
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,

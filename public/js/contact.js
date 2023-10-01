df4a20b1cce60cf45039f4950e6e8749ecbc1399
SKhai77 <orionkhai@gmail.com>
2023-09-30 21:35:18 -0500

    Created contact.js file

---
+++ public/js/contact.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Function to handle the form submission
+    const contactFormHandler = async (event) => {
+      event.preventDefault();
+  
+      // Get the updated title, content, and location values from the form
+      const email = document.querySelector('#email-contact').value.trim();
+      const name = document.querySelector('#name-contact').value.trim();
+      const message = document.querySelector('#message-contact').value.trim();
+  
+      // Retrieve the post ID from the URL
+      const postId = window.location.pathname.split('/').pop();
+  
+      // Send a PUT request to update the post
+      const response = await fetch(`/api/contact/`, {
+        method: 'POST',
+        body: JSON.stringify({ email, name, message }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      console.log(response);
+      if (response.ok) {
+        // Redirect to the updated post page
+        alert('Message submitted');
+      } else {
+        alert('Failed to update post');
+      }
+    };
+  
+    // Add an event listener for the form submission
+    const contactForm = document.querySelector('#contact-form');
+    if (contactForm) {
+      contactForm.addEventListener('submit', contactFormHandler);
+    }
+  });
+  
